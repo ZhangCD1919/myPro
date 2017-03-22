6d10df09eaeb0a7f9f7d17573d2183749280abed
ZhangCD1919 <zhangcaidie1919@163.com>
2017-03-22 10:30:39 +0800

    模块化处理完善登录功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,76 +1,36 @@
-
-define(['jquery','echarts','template','cookie'],function($,echarts,template){
-    
+define(['jquery','echarts','cookie'],function ($,echarts) {
     // 控制左侧导航菜单的显示和隐藏
     $('.navs ul').prev('a').on('click', function () {
         $(this).next().slideToggle();
     });
 
-    // 没有登录的时候跳转到登录页面
     var pathname = location.pathname;
+
     var flag = $.cookie('PHPSESSID');
-    if(!flag && pathname.indexOf('login') == -1){
-        // 没有登录
-        location.href = '/login'
+    if (!flag && pathname.indexOf('login') == -1) {
+        location.href = '/login';
     }
-    // 实现登录功能
-    $('#loginForm').submit(function(){
+// 登录功能
+    $("#loginForm").submit(function () {
         var formData = $(this).serialize();
         $.ajax({
-            type : 'post',
-            // url : 'http://api.studyit.com/login'
-            url : '/api/login',
-            data : formData,
-            dataType : 'json',
-            success : function(data){
-                if(data.code == 200){
-                    // console.log(data.result);
+            type: "post",
+            url: "/api/login",
+            data: formData,
+            dataType: 'json',
+            success: function (data) {
+                if (data.code == 200) {
                     var logInfo = JSON.stringify(data.result);
-                    // 实现cookie数据的跨页面共享
-                    $.cookie('logInfo',logInfo,{path : '/'});
-                    location.href = '/index/index'
-                }
-            },
-            error : function(data){
-                console.log(data.responseText);
-            }
-        });
-        return false;//阻止默认行为
-    });
-
-    // 退出功能
-    $('#logoutId').click(function(){
-        $.ajax({
-            type : 'post',
-            url : '/api/logout',
-            dataType : 'json',
-            success : function(data){
-                if(data.code == 200){
-                    location.href = '/login';
+                    $.cookie('logInfo', logInfo, {path: '/'});
+                    location.href = '/index/index';
                 }
             }
         });
+        return false;
     });
-
-    // var pathname = location.pathname;
-    // if(pathname.indexOf('login') == -1){
-    //     // 渲染登录信息
-    //     var obj = JSON.parse($.cookie('logInfo'));
-    //     // $('.aside .profile img').attr('src',obj.tc_avatar);
-    //     // $('.aside .profile h4').html(obj.tc_name);
-    //     var html = template('logInfo',obj);
-    //     $('.aside .profile').html(html);
-    // }
-    
-    var obj = JSON.parse($.cookie('logInfo'));
-    var tpl = '<div class="avatar img-circle">'+
-                  '<img src="{{tc_avatar}}">'+
-               '</div>'+
-               '<h4>{{tc_name}}</h4>';
-    var render = template.compile(tpl);
-    var html = render(obj);
-    $('.aside .profile').html(html);
-
-    
-    
+// 用户信息加载
+    var obj=JSON.parse($.cookie('logInfo'));
+    $('.aside .profile img').attr('src',obj.tc_avatar);
+    $('.aside .profile h4').html(obj.tc_name);
 });
+
--- public/js/echarts.js
+++ public/js/echarts.js
@@ -1,30 +1,26 @@
-
-define(['echarts','jquery','util'],function(echarts,$,util){
-    // 设置菜单选中
-    util.setMenu(location.pathname);
-
-    // 基于准备好的dom，初始化echarts实例
-    var myChart = echarts.init(document.getElementById('main'));
-    // 指定图表的配置项和数据
-    var option = {
-        title: {
-            text: 'ECharts 入门示例'
+/**
+ * Created by NINI on 2017/3/22.
+ */
+define(['echarts'],function (echarts) {
+    // 数据图
+    var myChart=echarts.init(document.getElementById("main"));
+    var option={
+        title:{
+            text:"eCharts入门"
         },
-        tooltip: {},
-        legend: {
-            data:['销量']
+        tooltip:{},
+        legend:{
+            data:['课程']
         },
-        xAxis: {
-            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        xAxis:{
+            data:["html","css","javaScript"]
         },
-        yAxis: {},
-        series: [{
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
+        yAxis:{},
+        series:[{
+            name:'课时',
+            type:"bar",
+            data:[5,20,14]
         }]
     };
-
-    // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option);
 });
